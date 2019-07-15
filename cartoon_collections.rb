522ad40f067a5a38d3566addbbaeca2cde169f71
Nicole Dow <nikki.dow71@gmail.com>
2019-07-15 22:03:01 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,8 +11,8 @@ def summon_captain_planet(calls)
   end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(short_calls)
+  
 end
 
 def find_the_cheese# code an argument here
