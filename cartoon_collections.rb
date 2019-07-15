21387728b6c03edc0fe88b74f09f459bf854bba4
Nicole Dow <nikki.dow71@gmail.com>
2019-07-15 22:01:51 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -9,7 +9,7 @@ def summon_captain_planet(calls)
   calls.map do |call|
     call = "#{call.capitalize}!"
   end
-  exclamations
+  #exclamations
 end
 
 def long_planeteer_calls# code an argument here
