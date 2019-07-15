0e25c5ed4bd15c5e2873c626c3cad74fe4ef626e
Nicole Dow <nikki.dow71@gmail.com>
2019-07-15 22:06:11 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -15,7 +15,8 @@ def long_planeteer_calls(calls)
   calls.any? {|call| call.size > 4}
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(strings)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  strings.find {|string| string.length 
 end
